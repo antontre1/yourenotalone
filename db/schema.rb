1534d9955ccf0b0e24c6ae068c1fecb8508fb91c
Charles De Freminville <cdefreminville@lucca.fr>
2022-01-29 16:28:29 +0100

    Enum ttry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_115507) do
+ActiveRecord::Schema.define(version: 2022_01_29_144352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2022_01_29_115507) do
     t.string "username"
     t.bigint "state_id", null: false
     t.boolean "admin"
+    t.text "status", default: "0", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["state_id"], name: "index_users_on_state_id"
