743a6a535589ea09bc6b33ec2a5378c4ce9e0ded
Charles De Freminville <cdefreminville@lucca.fr>
2022-01-29 16:46:38 +0100

    Text for States

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_144352) do
+ActiveRecord::Schema.define(version: 2022_01_29_154508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_01_29_144352) do
     t.integer "emotional_state"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "text"
   end
 
   create_table "themes", force: :cascade do |t|
