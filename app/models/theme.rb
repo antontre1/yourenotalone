2525771d1225b5c4b7810b1da815bfbad86354ce
virginie lty <virginielty@gmail.com>
2022-01-29 13:12:29 +0100

    nombre de caractères comment et theme

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,5 +2,5 @@ class Theme < ApplicationRecord
   has_many :articles
   has_many :favorites, as: :favoritable
 
-  validates :title, length: { minimum: 2, maximum: 500 }
+  validates :title, length: { minimum: 2, maximum: 75 }
 end
