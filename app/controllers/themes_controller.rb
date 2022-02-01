03d49bdf5f9ef04faa17d2193bc048db52e2504d
Antonin Folliasson <antonin@aitio.co>
2022-02-01 20:34:20 +0100

    theme/id/articles#index

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -2,4 +2,9 @@ class ThemesController < ApplicationController
   def index
     @themes = policy_scope(Theme)
   end
+
+  def show
+    @theme = Theme.find(params[:id])
+    authorize @theme
+  end
 end
