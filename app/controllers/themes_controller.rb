713f7dddbed43fbc00404eef4773fd1d440dd3d2
Antonin Folliasson <antonin@aitio.co>
2022-02-03 10:10:12 +0100

    ajout de liens

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,6 +1,7 @@
 class ThemesController < ApplicationController
   def index
     @themes = policy_scope(Theme)
+    @favorites = current_user.favorites.where(favoritable_type: "Theme")
   end
 
   def show
