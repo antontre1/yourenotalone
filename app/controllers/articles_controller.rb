33b4fc0d43ae5b5ab27479b24e579b1d5334a2e1
antontre1 <antontre1@users.noreply.github.com>
2022-02-05 12:42:35 +0100

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,7 +55,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :theme_id, :description, :content)
+    params.require(:article).permit(:title, :theme, :description, :content)
   end
 
 end
