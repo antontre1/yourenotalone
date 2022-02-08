ed8ae8bcc035942cd54240e091d2ec8ef014f107
Antonin Folliasson <antonin@aitio.co>
2022-02-08 17:48:58 +0100

    add biography to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_153244) do
+ActiveRecord::Schema.define(version: 2022_02_07_105954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 2022_02_03_153244) do
     t.bigint "state_id", null: false
     t.boolean "admin"
     t.text "status", default: "0", null: false
+    t.text "biography"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["state_id"], name: "index_users_on_state_id"
