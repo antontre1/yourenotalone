70c0afd9d5b17e98e89f2823607adc82112be36c
Antonin Folliasson <antonin@aitio.co>
2022-02-12 09:22:20 +0100

    avancé sur front dashboard

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # below we let possible connexion from ngrok tunnel
+  config.hosts << '.ngrok.io'
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
