ff764a5bce79168f9949abd1f7304d84b8644443
Antonin Folliasson <antonin@aitio.co>
2022-02-12 20:16:19 +0100

    ok pour navbar new design sans lien

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,13 +1,24 @@
+// const animNavbar = () => {
+//   const mon_avatar = document.querySelector('.avatar-listen');
+//   mon_avatar.addEventListener('click', toggleClassPopUp );
+// }
+
+// function toggleClassPopUp() {
+//   const menu = document.querySelector('.my-navbar-collapse');
+//   menu.classList.toggle("scale-1");
+// }
+
+// export { animNavbar };
+
 const animNavbar = () => {
-  const mon_avatar = document.querySelector('.avatar-listen');
-  console.log(mon_avatar);
+  const mon_avatar = document.querySelector('.me-logo');
   mon_avatar.addEventListener('click', toggleClassPopUp );
+  console.log(mon_avatar)
 }
 
 function toggleClassPopUp() {
-  const menu = document.querySelector('.my-navbar-collapse');
-  menu.classList.toggle("scale-1")
-  console.log(menu);
+  const menu = document.querySelector('.mon_menu');
+  menu.classList.toggle("scale-1");
 }
 
 export { animNavbar };
