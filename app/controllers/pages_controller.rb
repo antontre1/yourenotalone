bcae49b8b64889c57cedfe8abd75a2e53ea7072f
Antonin Folliasson <antonin@aitio.co>
2022-02-15 21:50:09 +0100

    dashboard in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,13 @@ class PagesController < ApplicationController
 
   def dashboard
     @themes = current_user.favorites.where(favoritable_type: "Theme").limit(3)
+    @thems_nb = current_user.favorites.where(favoritable_type: "Theme").count
     @articles = current_user.favorites.where(favoritable_type: "Article").limit(3)
-    @follow_users = current_user.favorites.where(favoritable_type: "User").limit(3)
+    @articles_nb = current_user.favorites.where(favoritable_type: "Article").count
+    @follow_users = current_user.favorites.where(favoritable_type: "User").limit(10)
+    @follow_users_nb = current_user.favorites.where(favoritable_type: "User").count
+    @follower_users_nb = Favorite.where(favoritable_type: "User", favoritable_id: current_user.id).count
+    @likes = Vote.where(user_id: current_user.id).count
   end
 
 end
