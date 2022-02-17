30a026945b4a9ad469361771524b1a7d63456850
Antonin Folliasson <antonin@aitio.co>
2022-02-17 19:47:57 +0100

    initwall

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,4 +16,15 @@ class PagesController < ApplicationController
     @likes = Vote.where(user_id: current_user.id).count
   end
 
+  def wall
+    @themes = current_user.favorites.where(favoritable_type: "Theme").limit(10)
+    @thems_nb = current_user.favorites.where(favoritable_type: "Theme").count
+    @articles = current_user.favorites.where(favoritable_type: "Article").limit(3)
+    @articles_nb = current_user.favorites.where(favoritable_type: "Article").count
+    @follow_users = current_user.favorites.where(favoritable_type: "User").limit(10)
+    @follow_users_nb = current_user.favorites.where(favoritable_type: "User").count
+    @follower_users_nb = Favorite.where(favoritable_type: "User", favoritable_id: current_user.id).count
+    @likes = Vote.where(user_id: current_user.id).count
+  end
+
 end
