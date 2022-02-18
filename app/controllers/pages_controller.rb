ed9b8060f0f1a2c41283ecd7d9ea22d566cb13d6
Antonin Folliasson <antonin@aitio.co>
2022-02-18 18:28:10 +0100

    ok pour recherches simples simultannees

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,22 +17,38 @@ class PagesController < ApplicationController
   end
 
   def wall
+    # liste des thèmes trendy
+    @scope = "Tendances"
+    @list_themes = Favorite.where(favoritable_type: "Theme").group("favoritable_id").count.sort_by{|_,v| -v}
 
-    @themes = current_user.favorites.where(favoritable_type: "Theme").limit(10)
-    @thems_nb = current_user.favorites.where(favoritable_type: "Theme").count
+    @themes = Array.new
+    @list_themes.each do |item|
+      @themes << Theme.find(item[0])
+    end
+
+
     @articles = current_user.favorites.where(favoritable_type: "Article").limit(3)
-    @articles_nb = current_user.favorites.where(favoritable_type: "Article").count
-    Favorite.where(favoritable_type: "User").group("favoritable_id").count.sort_by{|_,v| -v}
-    @list_users =
-    @follower_users_nb = Favorite.where(favoritable_type: "User", favoritable_id: current_user.id).count
-    @likes = Vote.where(user_id: current_user.id).count
+
+    # liste des users trendy
+    @table_ranked_users = Favorite.where(favoritable_type: "User").group("favoritable_id").count.sort_by{|_,v| -v}
+
+    @users = Array.new
+    @table_ranked_users.each do |item|
+     @users << User.find(item[0])
+    end
+
   end
 
   def search
-    @themes = Theme.where(title: params[:query])
-    @articles = Article.where(title: params[:query])
-    @list_users = User.where(username: params[:query])
-    render :wall
+    @scope = "d'intérêt"
+    if params[:query].present?
+      @themes = Theme.where(title: params[:query])
+      @articles = Article.where(title: params[:query])
+      @users = User.where(username: params[:query])
+      render :wall
+    else
+      redirect_to action: "wall"
+    end
   end
 
 end
